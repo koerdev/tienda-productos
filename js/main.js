2bcece10ea3d77a0f1681caa4ee894f456c06aa3
David Cornejo <koerdev@gmail.com>
2026-02-12 22:24:35 -0300

    feat: se agrega la funcionalidad para borrar producto con botón

--- js/main.js
+++ js/main.js
@@ -26,8 +26,10 @@ class UI {
         document.getElementById('product-form').reset();
     };
 
-    deleteProduct() {
-
+    deleteProduct(element) {
+        if (element.name === "delete") {
+            element.parentElement.parentElement.remove();
+        }
     };
 
     showMessages() {
@@ -48,4 +50,10 @@ document.getElementById('product-form').addEventListener("submit", (e) => {
 
     ui.addProduct(product);
     ui.resetForm();
+    ui.showMessages("Producto Agregado", "success");
+});
+
+document.getElementById("product-table-body").addEventListener("click", (e) => {
+    const ui = new UI();
+    ui.deleteProduct(e.target);
 });
